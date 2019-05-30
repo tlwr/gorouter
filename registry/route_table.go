f63c01f754fd89d0a2a97f9d8a18835beb04376f
Bruce Ricard <bricard@pivotal.io>
2019-05-30 11:21:43 -0700

    wip2

--- registry/route_table.go
+++ registry/route_table.go
@@ -2,8 +2,7 @@
 import "code.cloudfoundry.org/gorouter/route"
 
 type RouteTable interface {
-	Find(uri route.Uri) *route.Pool
-	Register(uri, endpoint)
-	Insert(uri route.Uri, endpoint *route.Endpoint)
-	Delete(uri route.Uri, endpoint *route.Endpoint)
+	Find(uri route.Uri) *route.Pool                 // lookup
+	Insert(uri route.Uri, endpoint *route.Endpoint) // register
+	Delete(uri route.Uri, endpoint *route.Endpoint) // deregister
 }
